4d2a773ba5b3614e6283cff2a1a1543881b0ffeb
wjenaro <wjenaro@gmail.com>
2024-02-08 21:49:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# redux-hello-world
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/wjenaro/redux-hello-world)
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+import { useState } from 'react';
+
+export default function App() {
+  // State: a counter value
+  const [counter, setCounter] = useState(0);
+
+  // Action: code that causes an update to the state when something happens
+  const increment = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  // View: the UI definition
+  return (
+    <div>
+      Value: {counter} <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
