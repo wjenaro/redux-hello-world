505e02c739a6b4b3609dd1cce2036957a61828de
wjenaro <wjenaro@gmail.com>
2024-02-08 22:10:18 +0300

    counter

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ export default function App() {
   const [counter, setCounter] = useState(0);
 
   // Action: code that causes an update to the state when something happens
+  const increment = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
 
   // View: the UI definition
   return (
